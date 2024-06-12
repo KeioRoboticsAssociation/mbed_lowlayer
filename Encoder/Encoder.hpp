5aea7beda7b80e40d7a8843aabd7d6870ffc45d9
Ryosuke Shimizu <52810488+ImANoob1122@users.noreply.github.com>
2024-06-12 17:45:38 +0900

    Update Encoder.hpp

--- Encoder/Encoder.hpp
+++ Encoder/Encoder.hpp
@@ -1,13 +1,13 @@
 #pragma once
-#ifndef ENCODER_H
-#define ENCODER_H
 
 #include "mbed.h"
+#include <vector>
+using namespace std;
 
 class Encoder {
 public:
     // コンストラクタ
-    Encoder(PinName pinA, PinName pinB, float pulse);
+    Encoder(PinName pinA, PinName pinB, int pulse);
 
     // 角速度を計算するメソッド
     float get_angular_velocity();
@@ -17,7 +17,7 @@ private:
     // 割り込みピン
     InterruptIn _encoderA;
     InterruptIn _encoderB;
-    float _pulse;
+    int _pulse;
 
     // エンコーダーの位置と計時用タイマー
     volatile int _position;
@@ -31,4 +31,21 @@ private:
     float _calculate_angular_velocity();
 };
 
-#endif
+class EncoderController {
+public:
+    //コンストラクタ
+    EncoderController(int pulse);
+
+    Encoder* addEncoder(PinName pinA, PinName pinB);
+
+    //すべてのモーターの角速度を入手するメソッド
+    vector<float> get_all_angular_velocity();
+
+    vector<float> get_inrange_angular_velocity(int start, int end);
+
+    float get_angular_velocity(Encoder* encoder);
+
+private:
+    int _pulse;
+    vector<Encoder*> encoders;
+};
